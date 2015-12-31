48828eabe7b7e2c816a38707c467c71e4bbf7422
Shawn Lasky <petryuno001@gmail.com>
2015-12-30 17:03:07 -0700

    work on taskitem and childitem Scope directive logic

--- task-it/public/javascripts/directives.js
+++ task-it/public/javascripts/directives.js
@@ -1,4 +1,5 @@
 taskItApp.directive('taskitem', [function(){
+    console.log("master item");
     return {
         restrict : 'EAC',
         template: '<ng-include src="template" />',
@@ -10,6 +11,34 @@ taskItApp.directive('taskitem', [function(){
             } 
             else if (scope.item.type == 'container') {
                 scope.template = '/web/directives/containerDirective.html';
+            }
+        }
+        
+    };
+    
+}]);
+
+taskItApp.directive('childitem', [function(){
+    console.debug("child item ");
+    return {
+        restrict : 'EAC',
+        template: '<ng-include src="template" />',
+        replace: true,
+        scope: {
+            child: '=',
+            type: '@',
+            title: '@'
+        },
+        link: function postLink(scope){
+            if (scope.child.type == 'task'){
+                scope.template = '/web/directives/taskDirective.html';
+                 
+            } 
+            else if (scope.child.type == 'container') {
+                scope.template = '/web/directives/containerDirective.html';
+            }
+            else if (scope.child.type == 'collaboration'){
+                scope.template = '/web/directives/collaborationDirective.html';
             }
         }
         
--- task-it/public/javascripts/task-it.js
+++ task-it/public/javascripts/task-it.js
@@ -8,9 +8,12 @@ taskItApp.controller('bodyController',['$scope', function($scope){
 	$scope.homeView = true; //default the condensor class toggle to home page
     
     $scope.masterList = [
-		{ID: '001', type: 'task', description: 'this is a task item description', parent: 'tainer1', title: 'Test1', children : [], countUnfinished: 1, status: {statusText: 'open'}, dueDate: '01/01/2016', assignee: [{name:'user1'},{name:'user2'}] },
-        {ID: '002', type: 'container', description: 'this is a container item', parent: '', title: 'Tainer1', children: ['002'], countUnfinished: 2,  status: {statusText: 'open'}, assignee: [{name: 'user1'},{name:'user2'}]},
-        {ID: '003', type: 'task', description: 'this is another task item description', parent: 'tainer1', title: 'Test Two', children : [], countUnfinished: 0, status: {statusText: 'closed'}, dueDate: '01/02/2016', assignee: [{name:'user1'}] }	
+         {ID: '001', type: 'container', description: 'this is a container item', parent: '', title: 'Project Container 1', children: [
+             {ID: '002', type: 'task', description: 'this is a task item description', parent: 'tainer1', title: 'Test1', resolved: false, status: {statusText: 'open'}, dueDate: '01/01/2016', assignee: [{name:'user1'},{name:'user2'}] }
+         ], countUnfinished: 1,  status: {statusText: 'open'}, assignee: [{name: 'user1'},{name:'user2'}]},
+         
+        
+        {ID: '003', type: 'task', description: 'this is another task item description', parent: 'tainer1', title: 'Test Two', resolved: false, status: {statusText: 'closed'}, dueDate: '01/02/2016', assignee: [{name:'user1'}] }	
 		
 	  ];
 	
